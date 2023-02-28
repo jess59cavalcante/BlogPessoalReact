9656bb6002e95f6acf56125c5a8ff14ec0dae185
Jessica Cavalcante <je.cavalcante@outlook.com>
2023-02-28 00:04:18 -0300

    FrontEndReact

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import react from "react";
+import"./Home.css";
+
+function Home(){
+    return(                 
+        <>
+        <h1>Home</h1>
+        <img src="https://imgur.com/uzNxkWd.png" alt="banner"
+        width= "1240" 
+        height="150"
+        className="img"/>
+        </>
+    )
+}
+export default Home;
