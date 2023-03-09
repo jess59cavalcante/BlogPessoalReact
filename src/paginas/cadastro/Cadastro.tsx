9c519abcdb38fa691e717267d67ff5ce912268a0
Jessica Cavalcante <je.cavalcante@outlook.com>
2023-03-08 23:57:32 -0300

    routers componentes 2

--- src/paginas/cadastro/Cadastro.tsx
+++ src/paginas/cadastro/Cadastro.tsx
@@ -1,48 +1,90 @@
-import React from 'react';
-import { Grid, Typography, TextField, Button } from '@material-ui/core';
-import {Box} from '@mui/material';
-import { Link } from 'react-router-dom';
-import './Cadastro.css';
-import { Login } from '@mui/icons-material';
+import React, { ChangeEvent, useEffect, useState} from 'react';
+import { Grid, Typography, Button, TextField } from '@material-ui/core';
+import { Box } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import User from '../models/User';
+import { cadastro } from '../services/Services';
+
 
 function Cadastro() {
+    let navigate = useNavigate();
+    const [confirmarSenha,setConfirmarSenha] = useState<String>("")
+    const [user, setUser] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            foto: '',
+            senha: ''
+        })
 
-    return (
-        
+    const [userResult, setUserResult] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            foto: '',
+            senha: ''
+        })
+
+    useEffect(() => {
+        if (userResult.id != 0) {
+            navigate("/login")
+        }
+    }, [userResult])
+
+
+    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>){
+        setConfirmarSenha(e.target.value)
+    }
+
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        if(confirmarSenha == user.senha){
+        await cadastro('/usuarios/cadastrar', user, setUserResult)
+        alert('Usuario cadastrado com sucesso')
+        }else{
+            alert('Dados inconsistentes. Favor verificar as informações de cadastro.')
+        }
+    }
+            return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box className='caixaformulario' padding={5}>
-                    <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='texto1' >Cadastre-se, é Rapidinho!!!!</Typography>
-                        <TextField className="ab"label="Nome" id="nome" name="nome" variant='outlined' fullWidth margin='normal' />
-                    <TextField className="ab" label="E-mail" id="email" name="email" variant='outlined' fullWidth margin='normal' />
-                    <TextField className="ab"label="Foto" id="foto" name="foto" variant='outlined' fullWidth margin='normal' />
-                    <TextField className="ab"label="Senha" type="password" id="senha" name="senha" variant='outlined' fullWidth margin='normal' />
-                    <TextField className="ab"label="Confirmar Senha" type="password" id="confirmarsenha" name="confirmarsenha" variant='outlined' fullWidth margin='normal' />
+                    <form onSubmit={onSubmit}>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Cadastrar</Typography>
+                        <TextField value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} className='ab'id='nome' label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
+                        <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}className='ab' id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}className='ab' id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)}className='ab' id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Cadastrar
+                            <Link to='/login' className='text-decorator-none'>
+                                <Button variant='contained' color='secondary' className='btnCancelar'>
+                                Cancelar
                                 </Button>
                             </Link>
+
+                            <Button type='submit' variant='contained' color='primary'>
+                                Cadastrar
+                            </Button>
                         </Box>
                     </form>
-                    <Grid >
-                    <Box  display='flex' justifyContent='center' marginTop={2}>
-                        <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Já é cadastrado?</Typography>
-                        </Box>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1' component={Link} to="/login">Faça Login</Typography>
-                    </Box>
-                    </Grid>
                 </Box>
-                
             </Grid>
-            <Grid xs={6} className='imagem'>
 
-            </Grid>
+
+
         </Grid>
-        
     );
 }
 
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
-import {Box} from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from 'react-use-localstorage';
+import UsuarioLogin from '../models/UserLogin';
+import { login } from '../services/Services';
 import './Login.css';
 
-function Login() {
 
-    return (
-        
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
+function Login() {
+  let navigate = useNavigate();
+  const [token, setToken] = useLocalStorage('token');
+  const [userLogin, setUserLogin] = useState<UsuarioLogin>({
+    id: 0,
+    nome: '',
+    usuario: '',
+    foto: '',
+    senha: '',
+    token:''
+  })
+  function updateModel(e: ChangeEvent<HTMLInputElement>) {
+    
+    setUserLogin({
+      ...userLogin,
+      [e.target.name]: e.target.value
+    })
+  }
+  useEffect(() => {
+    if(token !== '') {
+      navigate('/home')
+    }
+  }, [token])
+  
+  
+      
+  async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault()
+    try{
+      await login('/usuarios/logar', userLogin, setToken)
+      alert('Usuário logado com sucesso')
+    } catch(error) {
+      alert('Usuário e/ou senha inválidos')
+    }
+  }
+  return (
+    <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box className='caixaformulario' padding={5}>
-                    <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='texto1' >Para Visualizar o Conteudo,Faça o Login!!</Typography>
-                        <TextField id='usuario' className="ab" label='usuário'  variant='outlined'  name='usuario'  margin='normal'  fullWidth  />
+                <form onSubmit={onSubmit}>
 
-                        <TextField id='senha' className="ab" label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
-                        <Box marginTop={2} textAlign='center'>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='texto1' >Para Acessar o Conteúdo, Faça o Login!!</Typography>
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} className="ab" label="E-mail" id="email" name="usuario" variant='outlined' fullWidth margin='normal' />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} className="ab"label="Senha" type="password" id="senha" name="senha" variant='outlined' fullWidth margin='normal' />
+
+                                            <Box marginTop={2} textAlign='center'>
                             <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary'>
-                                    Logar
+                                    Login
                                 </Button>
                             </Link>
                         </Box>
@@ -27,9 +64,9 @@ function Login() {
                     <Grid >
                     <Box  display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem Cadastro?</Typography>
                         </Box>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1' component={Link} to="/cadastro">Cadastre-se</Typography>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1' component={Link} to="/cadastro">Cadastre Aqui</Typography>
                     </Box>
                     </Grid>
                 </Box>
@@ -39,8 +76,7 @@ function Login() {
 
             </Grid>
         </Grid>
-        
-    );
+  )
 }
 
 export default Login;
